95742d9364767f869bb739d56cc957c4728e3e00
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-07-23 15:57:30 +0300

    add mobile menu navigation

---
+++ src/js/mobMenu-nav.js
@@ -0,0 +1,11 @@
+const mobMenuNavBtn=document.querySelector(".nav-menu-btn");
+const mobMenu=document.querySelector(".mob-menu");
+const mobMenuCloseBtn=document.querySelector(".mob-menu-closeBtn");
+
+mobMenuNavBtn.addEventListener('click',()=>{
+    mobMenu.classList.remove("is-hidden")
+});
+
+mobMenuCloseBtn.addEventListener('click', ()=>{
+    mobMenu.classList.add("is-hidden")
+});
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,16 +1,8 @@
-// function simpleTemplating(data) {
-//     var html = '<ul>';
-//     $.each(data, function(index, item){
-//         html += '<li>'+ item +'</li>';
-//     });
-//     html += '</ul>';
-//     return html;
-// }
 
-// $('#pagination-container').pagination({
-//     dataSource: [1, 2, 3, 4, 5, 6, 7, ...  195],
-//     callback: function(data, pagination) {
-//         var html = simpleTemplating(data);
-//         $('#data-container').html(html);
-//     }
-// })
+
+
+
+
+
+
+
