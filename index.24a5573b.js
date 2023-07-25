4b0742cca22dd593a8f857c70b8e1709b96946f2
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2023-07-25 15:38:44 +0000

    Deploying to gh-pages from @ dimaparfenyk/school-vixon@ebf825d3c41b95dbc0400af1dcc30da4c828fc95 🚀

---
+++ index.24a5573b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".nav-menu-btn"),n=document.querySelector(".mob-menu"),t=document.querySelector(".mob-menu-closeBtn");e.addEventListener("click",(()=>{n.classList.remove("is-hidden")})),t.addEventListener("click",(()=>{n.classList.add("is-hidden")}));
+//# sourceMappingURL=index.24a5573b.js.map
