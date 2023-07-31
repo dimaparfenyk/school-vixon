2b20fe48de1f868ae197e49ec55e53772bc79d08
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2023-07-31 16:47:02 +0000

    Deploying to gh-pages from @ dimaparfenyk/school-vixon@ce41d3f573ec74c7456f32542bc1cf4fb86f4f76 🚀

--- index.283f466f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".nav-menu-btn"),n=document.querySelector(".mob-menu"),t=document.querySelector(".mob-menu-closeBtn");e.addEventListener("click",(function(){n.classList.remove("is-hidden")})),t.addEventListener("click",(function(){n.classList.add("is-hidden")}))}();
-//# sourceMappingURL=index.283f466f.js.map
