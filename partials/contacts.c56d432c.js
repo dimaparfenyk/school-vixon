804d85e58938b910e86f699528712c2fddfe9849
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2023-08-14 16:41:01 +0000

    Deploying to gh-pages from @ dimaparfenyk/school-vixon@67393298e91837c9a238009ce076ee7032f31428 🚀

---
+++ partials/contacts.c56d432c.js
@@ -0,0 +1,2 @@
+function myMap(){var e={center:new google.maps.LatLng(50.433921,30.511761),zoom:11};new google.maps.Map(document.getElementById("googleMap"),e)}
+//# sourceMappingURL=contacts.c56d432c.js.map
