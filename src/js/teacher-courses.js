75ff017ea6126a3dc60605aba8308ab23cb2bffb
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-08-21 23:25:38 +0300

    responsive teacher courses

--- src/js/teacher-courses.js
+++ src/js/teacher-courses.js
@@ -1,38 +1,43 @@
 import Pikaday from 'pikaday';
 let currentYear = new Date().getFullYear();
 
-var picker = new Pikaday({
-  field: document.getElementById('datepicker'),
-  //   format: 'D MMM YYYY',
-  bound: false,
-  yearRange: [currentYear, currentYear + 1],
+const courseCards = document.querySelectorAll('.course-card');
 
-  i18n: {
-    previousMonth: 'Предыдущий месяц',
-    nextMonth: 'Следующий месяц',
-    months: [
-      'Январь',
-      'Февраль',
-      'Март',
-      'Апрель',
-      'Май',
-      'Июнь',
-      'Июль',
-      'Август',
-      'Сентябрь',
-      'Октябрь',
-      'Ноябрь',
-      'Декабрь',
-    ],
-    weekdays: [
-      'Понедельник',
-      'Вторник',
-      'Среда',
-      'Четверг',
-      'Пятница',
-      'Суббота',
-      'Воскресенье',
-    ],
-    weekdaysShort: ['Пон', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-  },
+courseCards.forEach((card, index) => {
+  const datepickerInput = card.querySelector(`#datepicker${index + 1}`);
+  if (datepickerInput) {
+    const picker = new Pikaday({
+      field: datepickerInput,
+      bound: false,
+      yearRange: [currentYear, currentYear + 1],
+      i18n: {
+        previousMonth: 'Предыдущий месяц',
+        nextMonth: 'Следующий месяц',
+        months: [
+          'Январь',
+          'Февраль',
+          'Март',
+          'Апрель',
+          'Май',
+          'Июнь',
+          'Июль',
+          'Август',
+          'Сентябрь',
+          'Октябрь',
+          'Ноябрь',
+          'Декабрь',
+        ],
+        weekdays: [
+          'Понедельник',
+          'Вторник',
+          'Среда',
+          'Четверг',
+          'Пятница',
+          'Суббота',
+          'Воскресенье',
+        ],
+        weekdaysShort: ['Пон', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+      },
+    });
+  }
 });
