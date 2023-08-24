5df92380e73693ee1d900f79489bbb6a41d2f90c
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-08-24 20:48:51 +0300

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  backdrop: document.querySelector('.backdrop'),
+  closeModalBtnList: [...document.querySelectorAll('[data-modal-close]')],
+  openModalBtn: document.querySelector('[data-open-modal]'),
+};
+
+refs.closeModalBtnList.map(btn => btn.addEventListener('click', toggleModal));
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.body.addEventListener('keydown', closeModalbyEsc);
+refs.backdrop.addEventListener('click', e => {
+  if (e.target === e.currentTarget) {
+    toggleModal();
+  }
+});
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+function closeModalbyEsc(e) {
+  let isHidden = refs.backdrop.classList.contains('is-hidden');
+  if (e.code === 'Escape' && !isHidden) {
+    toggleModal();
+  }
+}
