1f9c62a5e00cf6d3bec6533f9be3d34ba1865f12
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-09-06 17:24:24 +0300

    add current training page

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,11 @@ const refs = {
   body: document.querySelector('body'),
   backdrop: document.querySelector('.backdrop'),
   closeModalBtnList: [...document.querySelectorAll('[data-modal-close]')],
-  openModalBtn: document.querySelector('[data-open-modal]'),
+  openModalBtn: [...document.querySelectorAll('[data-open-modal]')],
 };
 
 refs.closeModalBtnList.map(btn => btn.addEventListener('click', toggleModal));
-refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.map(btn => btn.addEventListener('click', toggleModal));
 refs.body.addEventListener('keydown', closeModalbyEsc);
 refs.backdrop.addEventListener('click', e => {
   if (e.target === e.currentTarget) {
