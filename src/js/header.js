aaee84300568ea2d8a2f6252db14f1fda889b5de
Dmitrij <92674961+dimaparfenyk@users.noreply.github.com>
2023-09-07 16:40:56 +0300

    local improvements

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,22 @@
 const header = document.querySelector('.header');
-const pageHeaderHeight = header.getBoundingClientRect().height;
+const pagesLinks = document.querySelectorAll('.nav-link');
 
-document.body.style.paddingTop = `${pageHeaderHeight}px`;
+const makeHeaderFixed = () => {
+  const pageHeaderHeight = header.getBoundingClientRect().height;
+
+  document.body.style.paddingTop = `${pageHeaderHeight}px`;
+};
+
+const highlightCurPage = () => {
+  const currentUrl = window.location.href;
+
+  const curPage = [...pagesLinks].find(link => link.href === currentUrl);
+
+  if (!curPage) return;
+
+  curPage.setAttribute('aria-current', 'page');
+  curPage.addEventListener('click', e => e.preventDefault());
+};
+
+highlightCurPage();
+makeHeaderFixed();
